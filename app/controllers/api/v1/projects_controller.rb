a1bb8e2b4ffbc42938b7121ff0fdbb3239a94330
gabriel belo <gabrielrbelo@gmail.com>
2020-10-19 20:43:46 -0300

    project create action and routes created

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,16 +1,21 @@
 class Api::V1::ProjectsController < ApplicationController
-  before_action :set_project, only: [ :show, :update ]
+  before_action :set_project, only: [ :update ]
 
   def index
     render json: Project.all
   end
 
-  def show
-    render json: @project
-  end
-
   def update
     if @project.update(project_params)
+      render json: @project
+    else
+      render_error
+    end
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
       render json: @project
     else
       render_error
