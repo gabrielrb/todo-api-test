82f19345533e03356414f6225a06ab5b8b3ea8cd
gabriel belo <gabrielrbelo@gmail.com>
2020-10-19 17:28:43 -0300

    cors enabled; cors gem installed; seed created; project controller created; project and task's general routes added; project index action created;

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+3.times do
+  project = Project.new(name: %w[UNO dois Three].sample)
+  project.save!
+
+  Task.new(date: '1990-12-01',
+           description: 'blablabla',
+           status: 'canceled',
+           project: project).save!
+
+  Task.new(date: '2020-08-26',
+           description: 'blablablablablablablablablablablablablablabla',
+           status: 'done',
+           project: project).save!
+
+  Task.new(date: '2000-06-10',
+           description: 'lots of things to do',
+           status: 'ongoing',
+           project: project).save!
+end
