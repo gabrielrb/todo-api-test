8c73ba72aa2aec327f2e75002be070cc6c71d497
gabriel belo <gabrielrbelo@gmail.com>
2020-10-21 18:14:05 -0300

    tests: project model and fixtures created

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -4,4 +4,8 @@ class ProjectTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def test_valid
+    assert projects(:one).valid?
+    refute Project.new.valid?
+  end
 end
